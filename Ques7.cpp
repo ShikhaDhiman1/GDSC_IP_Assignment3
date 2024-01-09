4d7bfc46ddd39f1ef1fa88bcb3c506cfffea2768
Shikha Dhiman <dshikha4403@gmail.com>
2024-01-09 21:24:56 +0530

    modified code for question 7

--- Ques7.cpp
+++ Ques7.cpp
@@ -10,48 +10,6 @@ struct ListNode {
 };
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
-        if(!list1) return list2;
-        else if(!list2) return list1;
-
-        ListNode* head = NULL;
-        if(list1->val <= list2->val){
-            head = list1;
-            list1 = list1->next;
-        }
-        else{
-            head = list2;
-            list2 = list2->next;
-        }
-
-        ListNode* temp = head;
-        while(list1 && list2){
-            if(list1->val <= list2->val){
-                temp->next = list1;
-                list1 = list1->next;
-            }
-            else{
-                temp->next = list2;
-                list2 = list2->next;
-            }
-            temp = temp->next;
-        }
-
-        if(!list1) temp->next = list2;
-        else temp->next = list1;
-
-        return head;
-    }
-    ListNode* middle(ListNode* head){
-        ListNode* slow = head;
-        ListNode* fast = head;
-
-        while(fast != NULL && fast->next != NULL && fast->next->next != NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
     ListNode* merge(ListNode* list1, ListNode* list2){
         if(!list1) return list2;
         else if(!list2) return list1;
